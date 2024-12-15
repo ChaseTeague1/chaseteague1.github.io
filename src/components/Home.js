67252cb2a163b1d72fe3d27b3f65ee9f97ffe632
ChaseTeague <teaguechase@yahoo.com>
2024-12-15 18:38:54 -0500

    added in react icons for the download symbol along with tweaked some styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { IoCloudDownloadOutline} from "react-icons/io5";
 
 function Home(){
 
@@ -13,12 +14,15 @@ function Home(){
     }
 
     return (
-        <div>
+        <div className="flex flex-col items-center justify-center h-screen ml-72 mb-72">
             <h1 className="text-white text-3xl">HI! I'm Chase Teague</h1>
             <button
                 onClick={handleDownload}
-                className="bg-red-600 text-white rounded-xl p-2"
-            >Download Resume</button>
+                className="m-4 bg-black hover:bg-red-600 transition duration-500 border-red-600 border text-white rounded-2xl p-2 shadow-sm shadow-red-800 flex items-center"
+            >
+                Download Resume 
+                <span className="ml-2"><IoCloudDownloadOutline /></span>
+            </button>
             <p className="text-white">Full-Stack Software Engineer</p>
         </div>
     )
