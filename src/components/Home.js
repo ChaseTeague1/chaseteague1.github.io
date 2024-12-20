284c9119fc7d25427bde5cb8c92a7494a132ccc0
ChaseTeague <teaguechase@yahoo.com>
2024-12-19 22:44:13 -0500

    added new styles to text font and messed around with added in project display block, added a background image

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,7 +18,7 @@ function Home(){
     return (
         <div className="flex flex-col h-screen items-center">
             <img className="w-80 h-80 m-0 rounded-full mb-10 shadow-red-600 shadow-[0px_5px_40px_5px_rgba(0,0,0,0.3)]" src="/thumbnail.jpeg"/>
-            <h1 className="text-white text-3xl">HI! I'm Chase Teague</h1>
+            <h1 className="text-white text-5xl italic">Chase Teague</h1>
             <button
                 onClick={handleDownload}
                 className="m-4 bg-black hover:bg-red-600 transition duration-500 border-red-600 border text-white rounded-2xl p-2 shadow-sm shadow-red-800 flex items-center"
@@ -27,8 +27,8 @@ function Home(){
                 <span className="ml-2"><IoCloudDownloadOutline /></span>
             </button>
             <div className="flex">
-                <a target="_blank" className="border-white" href="https://github.com/ChaseTeague1"><FaGithub className="text-white text-2xl border-red-600 border rounded-full m-4 mt-0"/></a>
-                <a target="_blank" href="https://www.linkedin.com/in/chaseteague/"><FaLinkedin className="text-white text-2xl border-red-600 m-4 mt-0"/></a>
+                <a target="_blank" className="border-white" href="https://github.com/ChaseTeague1"><FaGithub className="text-white text-2xl hover:text-red-600 rounded-full m-4 mt-0"/></a>
+                <a target="_blank" href="https://www.linkedin.com/in/chaseteague/"><FaLinkedin className="text-white text-2xl hover:text-red-600 m-4 mt-0"/></a>
             </div>
             <p className="text-white text-5xl mb-30 font-zapfino">Full-Stack Software Engineer</p>
         </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Project from "./Project";
 
 function Projects(){
     return (
-        <div>
-            <h1>Project page</h1>
+        <div className="flex flex-col justify-center w-screen">
+            <h1 className="text-white text-5xl mb-8 text-center">Projects</h1>
+            <div className="bg-gray-600 w-3/4 rounded-lg h-52 mx-auto flex flex-col justify-center">
+                <h1 className="text-white text-center m-0 text-3xl">I-RATED</h1>
+                <div className="bg-gray-800 h-32 w-5/6 rounded-lg mx-auto">
+
+                </div>
+            </div>
         </div>
     )
 }
